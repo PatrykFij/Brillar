30de9671e5f32b8c1f8f14eeb8af250d33b69b91
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2021-05-02 17:17:06 +0200

    Added GraphCMG api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Brillar`,
+    description: `Brillar shop`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -26,6 +26,17 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "SWAPI",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "cosmetics",
+        // Url to query from
+        url: "https://api-eu-central-1.graphcms.com/v2/cko70jj73xjcl01xt84pda944/master",
       },
     },
     `gatsby-plugin-gatsby-cloud`,
