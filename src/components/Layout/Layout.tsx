08807ad7afb594c469b55e8458d8ce41c9cb1dd4
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2021-05-05 00:13:49 +0200

    Removed unused import

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Header from "../Header/Header";
-import "./layout.css";
 
 interface ILayout {
     children?: JSX.Element[] | JSX.Element;
